affe1e99491b2674ee040f019ab18f44761337af
Lincoln Bryant <lincoln@lincolnbryant.com>
2026-01-02 20:02:45 -0500

    revert name

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Computing on the far side",
+    pageTitle: "computing on the far side",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -34,9 +34,9 @@ const config: QuartzConfig = {
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          secondary: "#386D73",
+          tertiary: "#385A73",
+          highlight: "rgba(168, 158, 213, 0.15)",
           textHighlight: "#fff23688",
         },
         darkMode: {
@@ -45,9 +45,9 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          secondary: "#558EAD",
+          tertiary: "#5572AD",
+          highlight: "rgba(168, 158, 213, 0.15)",
           textHighlight: "#b3aa0288",
         },
       },
