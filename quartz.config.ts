1793057967812a4c33111b997710f85f982b5f5e
Lincoln Bryant <lincoln@lincolnbryant.com>
2026-03-01 16:45:06 -0500

    fix

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "computing on the far side",
+    pageTitle: "programs from parallel earths",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
